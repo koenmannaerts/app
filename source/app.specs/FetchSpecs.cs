1fe883634f7979c6fd8cae87c4fa2c4c3bc9e19f
koenmannaerts <koen.mannaerts@gmail.com>
2012-09-20 15:34:43 +0200

    Pushing new changes

---
+++ source/app.specs/FetchSpecs.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using Machine.Specifications;
+using app.utility.container;
+using developwithpassion.specifications.rhinomocks;
+using developwithpassion.specifications.extensions;
+
+namespace app.specs
+{
+  [Subject(typeof(FetchDependencies))]
+  public class FetchDepencies
+  {
+    public abstract class concern : Observes<IFetchDependencies>
+    {
+
+    }
+
+    public class when_fething_dependencies : concern
+    {
+      Establish c = () =>
+      {
+        depends.on<SomeDependency>(instance_of_SomeDependency);
+        
+      };
+      Because b = () =>
+        result = sut.an<SomeDependency>();
+
+      It should_return_an_implementation_of_the_depency = () =>
+        result.ShouldEqual(instance_of_SomeDependency);
+
+      static SomeDependency result;
+      static IFetchDependencies the_container_facade;
+      static SomeDependency instance_of_SomeDependency;
+    }
+  }
+
+  public class SomeDependency
+  {
+  }
+}
---
+++ source/app/utility/container/Container.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace app.utility.container
+{
+  public class FetchDependencies : IFetchDependencies
+  {
+    public Collaborator an<Collaborator>()
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
